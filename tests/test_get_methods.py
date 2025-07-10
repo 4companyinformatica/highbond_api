7bc9939a6160067ff21562efde20caaec3032cd7
Diego R. Sousa <diegorosariosousa@gmail.com>
2025-07-10 16:14:16 -0300

    Criado teste de Organização em 'tests'

--- tests/test_get_methods.py
+++ tests/test_get_methods.py
@@ -1,23 +1,26 @@
 import sys
 import os
 
-sys.path.append(f"{os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))}\\src")
+sys.path.append(f"{os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))}")
 
-from highbond_api_class import Highbond_API
+from src.highbond_api_class import Highbond_API
 from dotenv import load_dotenv
 import os
 import pathlib
 
+
+script_root = pathlib.Path(__file__).parent.resolve()
+
+print(script_root)
+
+if os.path.exists(".env"):
+    load_dotenv(".env")
 
 d1 = Highbond_API(
     token=os.environ.get("HB_TOKEN"),
     organization_id=os.environ.get("HB_ORGID"),
     server=os.environ.get("HB_SERVER")
 )
-
-script_root = pathlib.Path(__file__)
-
-load_dotenv(".env")
 
 def test_getAgents():
     response = d1.getAgents()
