29356075433d111a288aa6cbc32e03b757da964e
Diego R. Sousa <diegorosariosousa@gmail.com>
2025-07-10 14:41:14 -0300

    O mesmo do log anterior, só que de verdade

---
+++ tests/test_get_methods.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+sys.path.append(f"{os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))}\\src")
+
+from highbond_api_class import Highbond_API
+from dotenv import load_dotenv
+import os
+import pathlib
+
+
+d1 = Highbond_API(
+    token=os.environ.get("HB_TOKEN"),
+    organization_id=os.environ.get("HB_ORGID"),
+    server=os.environ.get("HB_SERVER")
+)
+
+script_root = pathlib.Path(__file__)
+
+load_dotenv(".env")
+
+def test_getAgents():
+    response = d1.getAgents()
+    print(response)
+    assert bool(response)
+    
